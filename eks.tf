b29d83fab00481859b0a97978784762524130b4b
mahmoud sabra <mahmoudsabraa25@gmail.com>
2023-07-24 03:16:56 +0300

    adding deplyment

--- eks.tf
+++ eks.tf
@@ -19,8 +19,52 @@ module "eks" {
 
       min_size     = 2
       max_size     = 3
-      desired_size = 3
+      desired_size = 2
     }
   }
 }
 
+# Deployment for the multi-container pod
+resource "kubernetes_deployment_v1" "multi_container_pod" {
+  metadata {
+    name      = "aspnet-deployment"
+  }
+
+  spec {
+    replicas = 3  # Set the desired number of replicas
+
+    selector {
+      match_labels = {
+        app = "aspnet"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "aspnet"
+        }
+      }
+
+      spec {
+        # app
+        container {
+          name  = "app"
+          image = "ma7moudsabra/aspnet:latest"
+          # Add container1-specific configurations here
+        }
+
+        # Container 2
+        container {
+          name  = "mssql"
+          image = "mcr.microsoft.com/mssql/server"
+          # Add container2-specific configurations here
+        }
+
+       
+
+        # Add volumes and volumeMounts if required
+      }
+    }
+  }
+}
--- provider.tf
+++ provider.tf
@@ -7,10 +7,41 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-#  exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     # This requires the awscli to be installed locally where Terraform is executed
-#     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#   }
+ exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
+module "eks-kubeconfig" {
+  source     = "hyperbadger/eks-kubeconfig/aws"
+  version = "2.0.0"
+  depends_on = [module.eks]
+  cluster_name =  module.eks.cluster_name
+}
+
+# resource "kubernetes_manifest" "aspnet-deployment" {
+#   yaml_body = file("Eks-Cluster/aspnet-app-deploy.yaml")
+# }
+# resource "kubernetes_manifest" "db-depl" {
+#   yaml_body = file("k8s/mssql-deployment.yaml")
+# }
+
+
+
+# resource "kubernetes_manifest" "db-secret" {
+#   yaml_body = file("k8s/mssql-secret.yaml")
+# }
+
+
+# resource "kubernetes_manifest" "db-service" {
+#   yaml_body = file("k8s/mssql-service.yaml")
+# }
+# resource "kubernetes_manifest" "db-pvolume" {
+#   yaml_body = file("k8s/mssql-pvc.yaml")
+# }
+
+# resource "kubernetes_manifest" "db-volume" {
+#   yaml_body = file("k8s/mssql-pv.yaml")
+# }
